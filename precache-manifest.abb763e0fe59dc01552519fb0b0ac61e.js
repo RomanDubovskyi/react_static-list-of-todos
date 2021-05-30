756e23cbabda4a11d5c977218074ff826951baba
romandubovskyi <rdubowskij@gmail.com>
2021-05-30 20:06:34 +0300

    Updates

---
+++ precache-manifest.abb763e0fe59dc01552519fb0b0ac61e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8538f4ad7241ff7cc94f28ce46a35f25",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9445d268e7f47a5b01c1",
+    "url": "./static/css/main.01ade9a9.chunk.css"
+  },
+  {
+    "revision": "486876fe5217784983bf",
+    "url": "./static/js/2.79d301ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.79d301ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9445d268e7f47a5b01c1",
+    "url": "./static/js/main.a5c462d8.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
